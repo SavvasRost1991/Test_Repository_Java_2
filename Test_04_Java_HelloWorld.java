9d77672889fde48e287a178688a18368fa13211f
Savvas Rostantis <savvasrostantis@hotmail.com>
2018-07-29 09:58:30 +0300

    Adding Test_04_Java_HelloWorld.java to New Branch Branch_Repository_Test_Repository_Java_3

---
+++ Test_04_Java_HelloWorld.java
@@ -0,0 +1,25 @@
+public class Test_04_Java_HelloWorld {
+
+	public static void main(String[] args){
+
+		for(int i=0;i<=10;i++){
+
+
+			System.out.println("Test number 4..."+i);
+
+		}
+
+		for(i=0;i<=100;i++){
+
+
+			System.out.println("Test number 4..."+i);
+
+		}
+
+
+	}
+
+
+
+
+}
